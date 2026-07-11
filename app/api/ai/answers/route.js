0883a0990ed47e8154239e4e9b6a9dab3a1b787b
singhadhiraj0503-pixel <singhadhiraj0503@gmail.com>
2026-07-11 16:34:11 +0530

    AI Answer api route

---
+++ app/api/ai/answers/route.js
@@ -0,0 +1,30 @@
+import handleError from "@/lib/error";
+import { ValidationError } from "@/lib/http-errors";
+import { AIAnswerSchema } from "@/lib/validation";
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
+import { mistral } from "@ai-sdk/mistral";
+
+export const POST = async (req) => {
+  const { question, content } = await req.json();
+
+  try {
+    const validatedData = AIAnswerSchema.safeParse({ question, content });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { text } = await generateText({
+      model: mistral("mistral-large-latest"),
+      prompt: `Generate a markdown-formatted response to this following question: ${question}. Base it on the provided content: ${content}`,
+      system:
+        "You are a helpful assistant that provides informative responses in markdown format. Use appropriate markdown syntax for headings, lists, code blocks, and emphasis where necessary. For code blocks, use short-form smaller case language identifiers (e.g., 'js' for JavaScript, 'py' for Python, 'ts' for TypeScript, 'html' for HTML, 'css' for CSS, etc. ) ",
+    });
+
+    return NextResponse.json({ success: true, data: text }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api");
+  }
+};
